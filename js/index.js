08a98e63115974d71ead9d521b35d75ac7c33e5b
Matt K <matthedud@hotmail.com>
2022-02-21 18:53:09 +0100

    finish

--- js/index.js
+++ js/index.js
@@ -22,6 +22,7 @@ if (driver.length > navigator.length) {
 }
 
 // Iteration 3: Loops
+
 let driverUpperCase = "";
 
 for (let i = 0; i < driver.length; i++) {
@@ -46,16 +47,24 @@ for (const letter of driver) {
 console.log(driverRevers);
 
 let i=0
+let validate = false // va servir dans le cas ou un nom est un raccourci de l'autre (eg. Alex et Alexandre)
 while(i<driver.length || i<navigator.length){
     if(driver[i]<navigator[i]){
         console.log("The driver's name goes first.");
+        validate= true
         break
     } else if(driver[i]>navigator[i]){
         console.log("Yo, the navigator goes first definitely.");
+        validate= true
         break
     }
-    i++
+    else i++
 }
+
 if(driver===navigator){
     console.log("What?! You both have the same name?");
+} else if(driver>navigator && !validate){
+    console.log("Yo, the navigator goes first definitely.");
+} else if(driver<navigator && !validate){
+    console.log("The driver's name goes first.");
 }
