57796c1cd7876906050ec52d04164ec2bb549031
Matt K <matthedud@hotmail.com>
2022-02-21 18:09:22 +0100

    Iteration 3: Loops

--- js/index.js
+++ js/index.js
@@ -8,12 +8,39 @@ console.log(navigator);
 // Iteration 2: Conditionals
 
 if (driver.length > navigator.length) {
-    console.log(`The driver has the longest name, it has ${driver.length} characters.`)
-} else if (driver.length < navigator.length){
-    console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters`)
+	console.log(
+		`The driver has the longest name, it has ${driver.length} characters.`
+	);
+} else if (driver.length < navigator.length) {
+	console.log(
+		`It seems that the navigator has the longest name, it has ${navigator.length} characters`
+	);
 } else {
-    console.log(`Wow, you both have equally long names, ${navigator.length} characters!.`)
+	console.log(
+		`Wow, you both have equally long names, ${navigator.length} characters!.`
+	);
 }
 
 // Iteration 3: Loops
+let driverUpperCase = "";
 
+for (let i = 0; i < driver.length; i++) {
+    driverUpperCase += driver[i].toUpperCase()
+    if(driver[i+1]){
+        driverUpperCase += " "
+    }
+    // if(i<driver.length-1){
+    //     driverUpperCase += " "
+    // }
+}
+console.log(driverUpperCase);
+
+
+
+let driverRevers = "";
+
+for (const letter of driver) {
+    driverRevers = letter + driverRevers
+}
+
+console.log(driverRevers);
